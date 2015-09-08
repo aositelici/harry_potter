39d8ab1ee0797a756d5507af3267b9ae29776db9
liya <liya_013@live.com>
2015-09-08 23:35:52 +0800

    add getDiscount

--- src/counter.js
+++ src/counter.js
@@ -1,15 +1,27 @@
 'use strict';
 
-var Scanner = require('./scanner');
-
+var Discount = require('./discount');
 function Counter(basket, scanner) {
   this.basket = basket;
   this.scanner = scanner;
 }
 
-Counter.prototype.scan = function(tag) {
+Counter.prototype.scan = function (tag) {
 
-  this.scanner.scan(tag);
-
-
+  var basketItem = this.scanner.scan(tag);
+  if (basketItem) {
+    this.basket.addBasketItem(basketItem);
+  }
 };
+
+Counter.prototype.getDiscount = function (price) {
+
+  var discounting = new Discount(this.basket.getBasketItems(), price);
+  var amount = discounting.getAmount();
+
+  var savedAmount = discounting.getDiscount();
+  var discount = amount - savedAmount;
+  return discount.toFixed(2);
+};
+
+module.exports = Counter;
