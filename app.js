80c18cf7db103d2c35db705c6fb40e8868251211
liya <liya_013@live.com>
2015-09-08 23:38:12 +0800

    give the input

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var main = require('./src/main');
+
+var tags = ['first',
+  'first',
+  'second',
+  'second',
+  'third',
+  'third',
+  'fourth',
+  'fifth'];
+
+main.main(tags);
