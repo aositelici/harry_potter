e71bbe308a8f1733979f96ea4f5920a2d94261e3
liya <liya_013@live.com>
2015-09-08 14:35:25 +0800

    finish basket

--- src/basket.js
+++ src/basket.js
@@ -1,10 +1,33 @@
 'use strict';
 
 var basketItem = require('./basket-item');
+
 function Basket() {
   this.basketItems = [];
 }
 
-Basket.prototype.addBasketItem = function() {
-  //TODO:
+Basket.prototype.addBasketItem = function (basketItem) {
+  var findingItem = this.findBasketItem(basketItem);
+  if (findingItem) {
+    findingItem.count += basketItem.count;
+  }
+  else {
+    this.basketItems.push(basketItem);
+  }
 };
+
+Basket.prototype.findBasketItem = function (basketItem) {
+  var result;
+
+  this.basketItems.forEach(function (oneItem) {
+    if (basketItem.book.name === oneItem.book.name) {
+      result = oneItem;
+    }
+  });
+
+  if (result) {
+    return result;
+  }
+};
+
+module.exports = Basket;
