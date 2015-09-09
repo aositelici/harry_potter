a0e13ec33547cde76ba6618d1ffd93eba60638a4
liya <liya_013@live.com>
2015-09-09 22:05:48 +0800

    add reduceCount and format2

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -3,3 +3,19 @@
 exports.formatData = function(data) {
   return data.toFixed(2);
 };
+
+exports.format2 = function(data) {
+  return Math.round(data * 100) / 100;
+};
+
+exports.reduceCount = function(basketItems) {
+
+  for (var i = 0; i < basketItems.length; i++) {
+    basketItems[i].count -= 1;
+
+    if (basketItems[i].count === 0) {
+      basketItems.splice(i, 1);
+      i--;
+    }
+  }
+};
