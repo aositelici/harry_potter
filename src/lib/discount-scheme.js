0890304e6124cd938035b79d3064f1776739e764
liya <liya_013@live.com>
2015-09-10 13:16:49 +0800

    seperete discount scheme here from discount

---
+++ src/lib/discount-scheme.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var DISCOUNT_SCHEME = {
+  ONE_BOOK: 8.00,
+  TWO_BOOKS: 2 * 8.00 * 0.95,
+  THREE_BOOKS: 3 * 8.00 * 0.90,
+  FOUR_BOOKS: 4 * 8.00 * 0.80,
+  FIVE_BOOKS: 5 * 8.00 * 0.75
+};
+
+module.exports = DISCOUNT_SCHEME;
