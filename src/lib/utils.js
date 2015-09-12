0dd8c3e0bc6d6be0097db0da3fb421159de6e149
liya <liya013@live.com>
2015-09-12 22:01:59 +0800

    delete unnecessary method

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,6 +4,12 @@ exports.formatData = function(data) {
   return data.toFixed(2);
 };
 
-exports.format2 = function(data) {
-  return Math.round(data * 100) / 100;
+exports.reduceCount = function(counts) {
+  for (var i = 0; i < counts.length; i++) {
+    counts[i] -= 1;
+    if (counts[i] === 0) {
+      counts.splice(i, 1);
+      i--;
+    }
+  }
 };
